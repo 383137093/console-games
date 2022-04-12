f9e0f3fdc0bc6e96385dbc65ecba3424be384b21
Ashes <383137093@qq.com>
2022-04-12 21:44:22 +0800

    add conversion between string and wstring

--- library/ashes/winapi.cpp
+++ library/ashes/winapi.cpp
@@ -1,6 +1,6 @@
 ﻿#include "winapi.h"
+#include <cwchar>
 #include <cassert>
-#include <tchar.h>
 
 namespace ashes { namespace win {
 
@@ -29,7 +29,46 @@ bool IsAtLocaleZN()
 	wchar_t name[LOCALE_NAME_MAX_LENGTH];
 	::GetUserDefaultLocaleName(name, static_cast<int>(std::size(name)));
 	return ::_wcsnicmp(name, L"ZH", 2) == 0;
+}
 
+std::wstring A2W(const std::string& str, UINT code_page)
+{
+	return A2W(str.data(), static_cast<int>(str.size()), code_page);
+}
+
+std::string W2A(const std::wstring& wstr, UINT code_page)
+{
+	return W2A(wstr.data(), static_cast<int>(wstr.size()), code_page);
+}
+
+std::wstring A2W(const char* str, int len, UINT code_page)
+{
+	len = (len < 0 ? static_cast<int>(std::strlen(str)) : len);
+	int wlen = ::MultiByteToWideChar(code_page, 0, str, len, nullptr, 0);
+	if (wlen <= 0)
+		return {};
+
+	std::wstring wstr(wlen, 0);
+	wlen = ::MultiByteToWideChar(code_page, 0, str, len, &wstr[0], wlen);
+	if (wlen != static_cast<int>(wstr.size()))
+		return {};
+
+	return wstr;
+}
+
+std::string W2A(const wchar_t* wstr, int wlen, UINT code_page)
+{
+	wlen = (wlen < 0 ? static_cast<int>(std::wcslen(wstr)) : wlen);
+	int len = ::WideCharToMultiByte(code_page, 0, wstr, wlen, nullptr, 0, 0, 0);
+	if (len <= 0)
+		return {};
+
+	std::string str(len, 0);
+	len = ::WideCharToMultiByte(code_page, 0, wstr, wlen, &str[0], len, 0, 0);
+	if (len != static_cast<int>(str.size()))
+		return {};
+	
+	return str;
 }
 
 }}
--- library/ashes/winapi.h
+++ library/ashes/winapi.h
@@ -10,6 +10,12 @@ std::string LoadModuleResource(const char* name, const char* type);
 
 bool IsAtLocaleZN();
 
+std::wstring A2W(const std::string& str, UINT code_page);
+std::string W2A(const std::wstring& wstr, UINT code_page);
+
+std::wstring A2W(const char* str, int len, UINT code_page);
+std::string W2A(const wchar_t* wstr, int wlen, UINT code_page);
+
 }}
 
 #endif
